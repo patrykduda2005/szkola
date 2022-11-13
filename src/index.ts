d56b91b3135de37c61a33c2fdc9312724a46d3b5
patrykduda2005 <patrykduda7@gmail.com>
2022-11-13 15:28:05 +0100

    dodano reacta

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import "./css.css";
 console.log("Hello World!");
+
+var dodaj = function() {
+    console.log("test");
+}
+
+
+
